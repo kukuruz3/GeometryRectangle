5b690489c2674cb0965c32e2a0be09720730575b
Сергей <@>
2026-03-03 20:00:37 +0300

    Добавьте файлы проекта.

---
+++ GeometryRectangle/Program.cs
@@ -0,0 +1,68 @@
+﻿using GeometryRectangle;
+
+        try
+        {
+            Console.WriteLine("=== Демонстрация работы с прямоугольником ===\n");
+
+            // Создание прямоугольника
+            Console.WriteLine("1. Создание прямоугольника с координатами (10, 20), шириной 30, высотой 15");
+            Rectangle rect = new Rectangle(10, 20, 30, 15);
+            Console.WriteLine(rect);
+            Console.WriteLine();
+
+            // Демонстрация свойств
+            Console.WriteLine("2. Свойства прямоугольника:");
+            Console.WriteLine($"   Периметр: {rect.Perimeter:F2}");
+            Console.WriteLine($"   Площадь: {rect.Area:F2}");
+            Console.WriteLine($"   Правая граница: {rect.Right:F2}");
+            Console.WriteLine($"   Нижняя граница: {rect.Bottom:F2}");
+            Console.WriteLine();
+
+            // Проверка принадлежности точки
+            Console.WriteLine("3. Проверка принадлежности точек:");
+            double testX = 25, testY = 30;
+            Console.WriteLine($"   Точка ({testX}, {testY}) {(rect.ContainsPoint(testX, testY) ? "принадлежит" : "не принадлежит")} прямоугольнику");
+
+            testX = 5; testY = 10;
+            Console.WriteLine($"   Точка ({testX}, {testY}) {(rect.ContainsPoint(testX, testY) ? "принадлежит" : "не принадлежит")} прямоугольнику");
+            Console.WriteLine();
+
+            // Перемещение прямоугольника
+            Console.WriteLine("4. Перемещение прямоугольника на (5, -3):");
+            rect.Move(5, -3);
+            Console.WriteLine(rect);
+            Console.WriteLine();
+
+            // Изменение размера
+            Console.WriteLine("5. Изменение размера на ширину 40, высоту 20:");
+            rect.Resize(40, 20);
+            Console.WriteLine(rect);
+            Console.WriteLine();
+
+            // Демонстрация обработки некорректных значений
+            Console.WriteLine("6. Попытка установить отрицательную ширину:");
+            try
+            {
+                rect.Width = -10;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"   Ошибка: {ex.Message}");
+            }
+            Console.WriteLine();
+
+            // Создание прямоугольника через свойства
+            Console.WriteLine("7. Создание прямоугольника через инициализацию свойств:");
+            Rectangle rect2 = new Rectangle(0, 0, 10, 10)
+            {
+                X = 15,
+                Y = 25
+            };
+            Console.WriteLine(rect2);
+            Console.WriteLine();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
---
+++ GeometryRectangle/Rectangle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryRectangle
+{
+    public class Rectangle
+    {
+        private double _x;          // Координата X левого верхнего угла
+        private double _y;          // Координата Y левого верхнего угла
+        private double _width;     
+        private double _height;
+
+        public Rectangle(double x, double y, double width, double height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Ширина должна быть положительным числом");
+                _width = value;
+            }
+        }
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Высота должна быть положительным числом");
+                _height = value;
+            }
+        }
+        public double Perimeter
+        {
+            get { return 2 * (_width + _height); }
+        }
+        public double Area
+        {
+            get { return _width * _height; }
+        }
+        public double Right
+        {
+            get { return _x + _width; }
+        }
+
+        public double Bottom
+        {
+            get { return _y + _height; }
+        }
+
+        public void Move(double deltaX, double deltaY)
+        {
+            _x += deltaX;
+            _y += deltaY;
+        }
+
+        public void Resize(double newWidth, double newHeight)
+        {
+            Width = newWidth;
+            Height = newHeight;
+        }
+
+        public bool ContainsPoint(double pointX, double pointY)
+        {
+            return pointX >= _x && pointX <= _x + _width &&
+                   pointY >= _y && pointY <= _y + _height;
+        }
+
+        public override string ToString()
+        {
+            return $"Прямоугольник [({_x:F2}, {_y:F2}) - ({Right:F2}, {Bottom:F2})], " +
+                   $"Ширина: {_width:F2}, Высота: {_height:F2}, " +
+                   $"Периметр: {Perimeter:F2}, Площадь: {Area:F2}";
+        }
+    }
+
+}
+
